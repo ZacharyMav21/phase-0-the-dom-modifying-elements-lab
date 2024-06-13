b99278e834bd3c431f411696a7265129563515d3
Zachary <zepm0122.zn@gmail.com>
2024-06-13 19:55:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const parentElement = document.body
+const childElement = document.main
+parentElement.remove(main)
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'is the champion'
+newHeader.id = 'victory'
